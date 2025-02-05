9756c17ab228377c5956f7323eed37291bac1ca6
DESKTOP-J861PM6 <markocupic24@gmail.com>
2025-02-05 23:39:56 +0100

    chore: fix bugs and finally set up local environment

--- src/error_manager/error_manager.cpp
+++ src/error_manager/error_manager.cpp
@@ -1,8 +1,10 @@
 #include "error_manager.h"
 
-void error_manager_handleError(error_manager_error_ts error_msg) {
+void error_manager_handleError(error_manager_error_ts error_msg)
+{
     error_manager_error_ts error_msg_serial = data_router_routeErrorToOutput(OUTPUT_SERIAL_CONSOLE, error_msg);
-    if (error_msg_serial != ERROR_CODE_NO_ERROR) {
+    if (error_msg_serial.error_code != ERROR_CODE_NO_ERROR)
+    {
         data_router_routeErrorToOutput(OUTPUT_DISPLAY, error_msg);
     }
 }
