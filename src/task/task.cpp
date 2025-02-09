36c27f295b1a3897e490bbb94dede80849d5e144
Mihajlo Sasic <sasic.mihajlo25@gmail.com>
2025-02-09 22:12:25 +0100

    NU1RmG7k - feat: Initial commit on task layer, create basic functionality

--- src/task/task.cpp
+++ src/task/task.cpp
@@ -1,5 +1,16 @@
 #include "task.h"
 
+static tasks_config_ts tasks_config[] =
+{
+  {millis(), TASK_I2C_ADDR_READ_TIMER, TASK_I2C_ADDR_READ},
+  {millis(), TASK_SENSOR_READ_TIMER, TASK_SENSOR_READ},
+  {millis(), TASK_TIME_READ_TIMER, TASK_TIME_READ}
+};
+
+static bool intervalPassed(uint8_t task_id);
+static uint8_t findTaskIndex(uint8_t task_id);
+static size_t getNumOfTasks();
+
 void task_initTask()
 {
   error_manager_error_code_te error_code = ERROR_CODE_NO_ERROR; // Default Error code if every init goes correct
@@ -23,30 +34,75 @@ void task_initTask()
 
 void task_cyclicTask()
 {
-  static sensor_reading_context_ts context1 = app_createNewSensorsReadingContext();
+  static i2cScan_reading_context_ts context_i2c_scan = app_createI2CScanReadingContext();
+  static sensor_reading_context_ts context_sensor_reading = app_createNewSensorsReadingContext();
+  static task_state_machine_te current_state = STATE_SCANNING_FOR_I2C_ADDRESSES;
 
-  static unsigned long previous_millis = 0;
-
-  unsigned long current_millis = millis();
-
-#ifdef MODE_GET_I2C_ADDR
-  if (current_millis - previous_millis >= (unsigned long)I2CSCAN_I2CSCAN_INTERVAL_MS)
+  if(STATE_SCANNING_FOR_I2C_ADDRESSES == current_state)
   {
-    previous_millis = current_millis;
-    i2cScan_scanForAdress();
+    if(INTERVAL_PASSED == intervalPassed(TASK_I2C_ADDR_READ))
+    {
+      if(FINISHED == app_readAllI2CAddressesPeriodic(ALL_OUTPUTS, &context_i2c_scan))
+      {
+        current_state = STATE_CYCLIC_SENSOR_AND_TIME_READING;
+      }
+    }
   }
-#else
-  if (current_millis - previous_millis >= (unsigned long)DISPLAY_TIME_DISPLAY_INTERVAL_MS)
+  else if(STATE_CYCLIC_SENSOR_AND_TIME_READING == current_state)
   {
-    previous_millis = current_millis;
-
-    app_readCurrentRtcTime(LCD_DISPLAY);
+    if(INTERVAL_PASSED == intervalPassed(TASK_SENSOR_READ))
+    {
+      (void)app_readAllSensorsPeriodic(ALL_OUTPUTS, &context_sensor_reading);
+    }
+    if(INTERVAL_PASSED == intervalPassed(TASK_TIME_READ))
+    {
+      (void)app_readCurrentRtcTime(LCD_DISPLAY);
+    }
   }
-  if (current_millis - previous_millis >= (unsigned long)DISPLAY_DISPLAY_INTERVAL_MS)
+
+  delay(CYCLIC_TASK_DELAY_MS);
+}
+
+static bool intervalPassed(uint8_t task_id)
+{
+  uint32_t current_milis = millis();
+  tasks_config_ts current_task;
+  uint8_t task_index = findTaskIndex(task_id);
+  if(TASK_INVALID_INDEX != task_index)
   {
-    previous_millis = current_millis;
-
-    (void)app_readAllSensorsPeriodic(ALL_OUTPUTS, &context1);
+    if(current_milis - tasks_config[task_index].previous_millis >= tasks_config[task_index].task_period)
+    {
+      tasks_config[task_index].previous_millis = current_milis;
+      return INTERVAL_PASSED;
+    }
   }
-#endif
+  return INTERVAL_NOT_PASSED;
+}
+
+static uint8_t findTaskIndex(uint8_t task_id)
+{
+  uint8_t index_returned = TASK_INVALID_INDEX;
+  size_t num_of_tasks = getNumOfTasks();
+
+  // Loop through all tasks till the right one is found
+  for (uint8_t index = TASK_FIRST_TASK_INDEX; index < num_of_tasks; index++)
+  {
+    if(task_id == tasks_config[index].task_id)
+    {
+      index_returned = index;
+      break;
+    }
+  }
+
+  return index_returned;
+}
+
+static size_t getNumOfTasks()
+{
+  size_t num_of_tasks = TASK_NO_TASKS; // Default value in case of no tasks configured
+  if (TASK_NO_TASKS < sizeof(tasks_config)) // Ensure there is at least 1 task
+  {
+    num_of_tasks = sizeof(tasks_config) / sizeof(tasks_config[TASK_FIRST_TASK_INDEX]);
+  }
+  return num_of_tasks;
 }
