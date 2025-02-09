57fcdc51360bd82e14d6eaa4654adab49bdf8670
Mihajlo Sasic <sasic.mihajlo25@gmail.com>
2025-02-09 11:21:05 +0100

    w7Ht2NPR - feat: Recreate app folder structure to modularize component applications

---
+++ src/app_layer/app_sensors/app_sensors.cpp
@@ -0,0 +1,94 @@
+#include "app_sensors.h"
+
+/* STATIC FUNCTION PROTOTYPES */
+/***
+ * Updates the sensor index cyclically.
+ * @param current_index The current sensor index.
+ * @param number_of_sensors The total number of sensors.
+ * @return The updated sensor index.
+ */
+static uint8_t readAllSensorsPeriodicUpdateSensorIndex(uint8_t current_index, size_t number_of_sensors);
+/* *************************************** */
+
+/* EXPORTED FUNCTIONS */
+task_status_te app_readSpecificSensor(uint8_t sensor_id, output_destination_t output)
+{
+    data_router_input_data_ts sensor_reading_result = data_router_fetchDataFromInput(INPUT_SENSORS, sensor_id); // Fetch data from the sensor
+    checkForErrors(sensor_reading_result.error_msg);
+    if(IS_OUTPUT_INCLUDED(output, LCD_DISPLAY))
+    {
+        checkForErrors(data_router_routeDataToOutput(OUTPUT_DISPLAY, sensor_reading_result)); // Send sensor data to display output
+    }
+    if(IS_OUTPUT_INCLUDED(output, SERIAL_CONSOLE))
+    {
+        checkForErrors(data_router_routeDataToOutput(OUTPUT_SERIAL_CONSOLE, sensor_reading_result)); // Send sensor data to serial console output
+    }
+    return FINISHED; // Return value is used to notify task component
+}
+
+task_status_te app_readAllSensorsPeriodic(output_destination_t output, sensor_reading_context_ts *context)
+{
+    // If sensors are available, process them one by one in a cyclic manner
+    if(SENSORS_INTERFACE_NO_SENSORS_CONFIGURED != context->number_of_sensors)
+    {
+        // Get sensor ID from the current index
+        uint8_t current_sensor_id = sensors_interface_sensorIndexToId(context->sensor_index);
+        // Process only valid sensor IDs
+        if(INVALID_SENSOR_ID != current_sensor_id)
+        {
+            (void)app_readSpecificSensor(current_sensor_id, output);
+        }
+        // Update sensor index for the next iteration
+        context->sensor_index = readAllSensorsPeriodicUpdateSensorIndex(context->sensor_index, context->number_of_sensors);
+    }
+
+    // If we have cycled through all sensors, indicate that processing is complete
+    return (STARTING_SENSOR_INDEX == context->sensor_index) ? FINISHED : NOT_FINISHED;
+}
+
+task_status_te app_readAllSensorsAtOnce(output_destination_t output)
+{
+    output = filterOutTimeDependentOutputs(output);
+    if(NO_OUTPUTS != output) // Check if all outputs are filtered out
+    {
+        size_t number_of_sensors = sensors_interface_getSensorsLen();
+
+        // If there are sensors configured, process each one
+        if(SENSORS_INTERFACE_NO_SENSORS_CONFIGURED != number_of_sensors)
+        {
+            // Loop through all sensor indices and process each one
+            for (uint8_t sensor_index = STARTING_SENSOR_INDEX; sensor_index < number_of_sensors; sensor_index++)
+            {
+                uint8_t current_sensor_id = sensors_interface_sensorIndexToId(sensor_index);
+                // Only process valid sensor IDs
+                if(INVALID_SENSOR_ID != current_sensor_id)
+                {
+                    (void)app_readSpecificSensor(current_sensor_id, output);
+                }
+            }
+        }   
+    }
+
+    return FINISHED; // Return FINISHED since all sensors are processed
+}
+
+sensor_reading_context_ts app_createNewSensorsReadingContext()
+{
+    sensor_reading_context_ts new_sensor_reading_context = {sensors_interface_getSensorsLen(), STARTING_SENSOR_INDEX};
+    return new_sensor_reading_context;
+}
+/* *************************************** */
+
+/* STATIC FUNCTIONS IMPLEMENTATIONS */
+static uint8_t readAllSensorsPeriodicUpdateSensorIndex(uint8_t current_index, size_t number_of_sensors)
+{
+    current_index++;
+    // Reset index if it exceeds the total number of sensors
+    if(current_index >= number_of_sensors) 
+    {
+      current_index = STARTING_SENSOR_INDEX;
+    }
+
+    return current_index;
+}
+/* *************************************** */
